56c2ff0596c640d87a0e6e0390751973aab482e6
Bryce Hadsell <bryce.hadsell@gmail.com>
2023-07-10 18:40:01 -0500

    Updates

--- Temporary Aliases/scratch.py
+++
@@ -1,5 +0,0 @@
-for gvar_str in variable:
-    gvar = load_json(get_gvar(gvar_str))
-    for x in gvar:
-        if help.processName(x.name).lower() == weapon.lower():
-            attacks.append(x)
