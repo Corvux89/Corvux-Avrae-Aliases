3424af59bee9018a1398c31fa2da2ed74aa2d22b
Bryce Hadsell <bryce.hadsell@gmail.com>
2023-07-27 16:46:45 -0500

    Updates

---
+++ TODO.py
@@ -0,0 +1,11 @@
+# TODO: Ships
+#   Boost
+#   Detect
+#   Direct
+#   Helm
+#   Interfere
+#   Role Specific Stuff (ship level)
+#   S-Foils
+#   BUGFIX: Tertiary and Quaternary Ammunition Scaling
+
+# TODO: Penetrating Weapons
