ed4d024ee32d7f88314c041f70a068f657ec8b35
Bryce Hadsell <bryce.hadsell@gmail.com>
2023-08-01 16:40:56 -0500

    Updates

--- Collections/SW5E Things/power/stats.py
+++ Collections/SW5E Things/power/stats.py
@@ -8,7 +8,7 @@ out_dict = {}
 out_dict["total"] = len(spells)
 out_dict["Force"] = {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0}
 out_dict["Tech"] = {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0}
-out_dict["dex"] = 0
+out_dict["automated"] = 0
 
 for sp in spells:
     try:
@@ -16,10 +16,9 @@ for sp in spells:
     except KeyError:
         print(f"Error with {sp}")
 
-    if 'description' in sp and "dexterity saving" in sp.get('description').lower():
-        out_dict["dex"] += 1
+    if sp.get("automation") and len(sp["automation"]) >=1:
+        out_dict["automated"] += 1
 
 
-print(f"Total Spells: {out_dict['total']}\nForce Powers: {out_dict['Force']}\nTech Power: {out_dict['Tech']}"
-      f"\nDex saves: {out_dict['dex']}")
+print(f"Total Spells: {out_dict['total']} ({out_dict['automated']} automated)\nForce Powers: {out_dict['Force']}\nTech Power: {out_dict['Tech']}")
 
