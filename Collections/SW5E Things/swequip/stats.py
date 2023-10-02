03abde2df0c93ba614b70ed4eee59c2422757549
corvux <bryce.hadsell@gmail.com>
2023-10-01 19:05:01 -0500

    All weapons setup

--- Collections/SW5E Things/swequip/stats.py
+++ Collections/SW5E Things/swequip/stats.py
@@ -2,9 +2,17 @@ import json
 import os
 
 total_count = 0
+w_list = []
 
 for filename in os.listdir('weapons'):
     f=open(os.path.join('weapons', filename), encoding='utf-8')
-    total_count += len(json.load(f))
+    weapons = json.load(f)
+    total_count += len(weapons)
+    w_list+=[w.get('name') for w in weapons]
+
+
+with open("all weapons.json", "w") as outfile:
+    json.dump(w_list, outfile)
+
 
 print(f"Total Weapons: {total_count}")
