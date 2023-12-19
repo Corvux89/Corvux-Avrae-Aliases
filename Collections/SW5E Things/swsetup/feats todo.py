144e270d807cb2b726dd1d9c844395ca6a1a53e5
Bryce Hadsell <bryce.hadsell@gmail.com>
2023-12-19 17:14:51 -0600

    Updates

---
+++ Collections/SW5E Things/swsetup/feats todo.py
@@ -0,0 +1,107 @@
+# TODO: Actor
+# TODO: Alert
+# TODO: Animal Handler
+# TODO: Armor Expert
+# TODO: Athlete
+# TODO: Augmented Cyborg
+# TODO: Battle Scarred
+# TODO: Bountiful Luck
+# TODO: Brawny
+# TODO: Charmer
+# TODO: Class Proficiencies
+# TODO: Climber
+# TODO: Commando
+# TODO: Competitor
+# TODO: Crafter
+# TODO: Customized Droid
+# TODO: Dungeon Delver
+# TODO: Durable
+# TODO: Empathic
+# TODO: Entertainer
+# TODO: Fanatic
+# TODO: Feigned Confidence
+# TODO: Fighting Master
+# TODO: Fighting Styles and Masteries
+# TODO: Fighting Stylist
+# TODO: Force Guidance
+# TODO: Force of Personality
+# TODO: Forceful Vigor
+# TODO: Formfighting Dabbler
+# TODO: Galvanizing Presence
+# TODO: Haggler
+# TODO: Healer
+# TODO: Inspiring Leader
+# TODO: Investigative Attunement
+# TODO: Investigator
+# TODO: Keen Mind
+# TODO: Linguist
+# TODO: Loremaster
+# TODO: Lucky
+# TODO: Mariner
+# TODO: Medic
+# TODO: Mobile
+# TODO: Muscled Menace
+# TODO: Naturalist
+# TODO: Observant
+# TODO: Perceptive
+# TODO: Performer
+# TODO: Power Adept
+# TODO: Power Channeling
+# TODO: Practiced
+# TODO: Promising Commander
+# TODO: Quick-Fingered
+# TODO: Quick-Witted
+# TODO: Resilient
+# TODO: Savage Shorty
+# TODO: Shard Modification
+# TODO: Snappy Interjection
+# TODO: Specialist
+# TODO: Stealthy
+# TODO: Supreme Accuracy
+# TODO: Supreme Aptitude
+# TODO: Supreme Durability
+# TODO: Survivalist
+# TODO: Techie
+# TODO: Tiny Terror
+# TODO: Tough
+# TODO: Weapon Expert
+# TODO: Weapon Focused
+# TODO: Weapon Supremacist
+# TODO: Adaptable Fading
+# TODO: Auditory Dampening
+# TODO: Blinding Agility
+# TODO: Casting Specialist
+# TODO: Chemical Buffer
+# TODO: Class Improvement
+# TODO: Close Quarters Caster
+# TODO: Combat Caster
+# TODO: Companion Keeper
+# TODO: Cunning Forcecaster
+# TODO: Cunning Intellect
+# TODO: Double-Weapon Casting
+# TODO: Dual Focused Caster
+# TODO: Dutifully Deployed
+# TODO: Electrical Grounding
+# TODO: Exalted Awareness
+# TODO: Exotic Weapon Training
+# TODO: Fighting Styles and Masteries
+# TODO: Flexible Rending
+# TODO: Force Frustrater
+# TODO: Heat Shielded
+# TODO: Metabolic Tolerance
+# TODO: Mindful Meditator
+# TODO: Mounted Caster
+# TODO: Multiclass Improvement
+# TODO: Overwhelming Presence
+# TODO: Quick Caster
+# TODO: Sniping Caster
+# TODO: Spiritual Techcaster
+# TODO: Splashclass Improvement
+# TODO: Titan's Power
+# TODO: Total Reconstruction
+# TODO: Two-Weapon Casting
+# TODO: Unnatural Resilience
+# TODO: Versatile Withering
+# TODO: Vigorous Virility
+# TODO: War Caster
+# TODO: Well-Insulated
